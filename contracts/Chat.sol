b620ccce6223c532c3a4426775276bc6eac06e1d
Lucas Sotelo <lucasnsotelog@gmail.com>
2025-05-02 23:28:59 -0300

    feat: alias for sender address

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./ChatFactory.sol";
+
 contract Chat {
     struct Message {
         bytes32 id;
         bytes32 parentId;
         address sender;
+        string senderAlias;
         string content;
         uint256 timestamp;
     }
@@ -14,11 +17,17 @@ contract Chat {
         bytes32 indexed id,
         bytes32 indexed parentId,
         address indexed sender,
+        string senderAlias,
         string content,
         uint256 timestamp
     );
 
     Message[] public messages;
+    ChatFactory public factory;
+
+    constructor() {
+        factory = ChatFactory(msg.sender);
+    }
 
     function sendMessage(string memory _content, bytes32 _parentId) public {
         // Generate a pseudo-unique ID for the message
@@ -30,20 +39,34 @@ contract Chat {
             id: messageId,
             parentId: _parentId,
             sender: msg.sender,
+            senderAlias: factory.getAlias(msg.sender),
             content: _content,
             timestamp: block.timestamp
         });
         messages.push(newMessage);
         emit MessageSent(
-            messageId,
-            _parentId,
-            msg.sender,
-            _content,
-            block.timestamp
+            newMessage.id,
+            newMessage.parentId,
+            newMessage.sender,
+            newMessage.senderAlias,
+            newMessage.content,
+            newMessage.timestamp
         );
     }
 
     function getMessages() public view returns (Message[] memory) {
-        return messages;
+        uint256 messageCount = messages.length;
+        Message[] memory allMessages = new Message[](messageCount);
+        for (uint256 i = 0; i < messageCount; i++) {
+            // Copy the message to the new array without referencing the original
+            allMessages[i].id = messages[i].id;
+            allMessages[i].parentId = messages[i].parentId;
+            allMessages[i].sender = messages[i].sender;
+            allMessages[i].content = messages[i].content;
+            allMessages[i].timestamp = messages[i].timestamp;
+            // Update the senderAlias for each message without modifying the original
+            allMessages[i].senderAlias = factory.getAlias(messages[i].sender);
+        }
+        return allMessages;
     }
 }
--- contracts/ChatFactory.sol
+++ contracts/ChatFactory.sol
@@ -7,15 +7,32 @@ contract ChatFactory {
     address[] public chats;
     string[] public chatNames;
     mapping(string => address) public chatNameToAddress;
+    mapping(address => string) public walletAddressToAlias;
 
     function createChat(string memory _name) public {
         require(
             chatNameToAddress[_name] == address(0),
             "Chat with this name already exists"
         );
+        require(bytes(_name).length > 0, "Chat name cannot be empty");
         Chat newChat = new Chat();
         chats.push(address(newChat));
         chatNames.push(_name);
         chatNameToAddress[_name] = address(newChat);
+    }
+
+    function setAlias(string memory _alias) public {
+        require(
+            bytes(walletAddressToAlias[msg.sender]).length == 0,
+            "Alias already exists"
+        );
+        require(bytes(_alias).length > 0, "Alias cannot be empty");
+        walletAddressToAlias[msg.sender] = _alias;
+    }
+
+    function getAlias(
+        address _walletAddress
+    ) public view returns (string memory) {
+        return walletAddressToAlias[_walletAddress];
     }
 }
